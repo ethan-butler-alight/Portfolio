ce53bb528734cd7ad4e1deadd4bb3c6e69c9dbea
ethanbtlr <4589948+ethanbtlr@users.noreply.github.com>
2022-06-12 22:02:51 -0400

    Worked on hamburger menu opening and closing

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburgerButton = document.getElementById('hamburger-button');
+    const contactButton = document.getElementById('contact-button')
 
     hamburgerButton.addEventListener('click', hamburgerMenu);
     
@@ -10,9 +11,30 @@ document.addEventListener('DOMContentLoaded', () => {
         if (navlinks.style.display != 'flex' ) {
             navlinks.style.display = 'flex';
             navlinks.style.flexDirection = 'column';
+            navlinks.style.position = 'absolute';
+            navlinks.style.top = '6vh';
+            navlinks.style.backgroundColor = "#2B2942";
+            navlinks.style.width = "284px";
+            navlinks.style.height = "94vh";
+            navlinks.style.zIndex = "1";
+            navlinks.style.justifyContent = 'start';
+            navlinks.style.rowGap = '24px';
+            navlinks.style.padding = '31px 0 0 31px';
+
+
+
+
+            // Remove CTA styling when using the mobile nav
+            contactButton.classList.remove('cta')
+
         }
+
+
         // If the nav is open
         else {
+            // Add CTA styling back for desktop
+            contactButton.classList.add('cta')
+
             navlinks.style.display = 'none';
         }
     }
