7b3fe66442a4a53805207b84fa71ab4bf3fee91d
ethanbtlr <4589948+ethanbtlr@users.noreply.github.com>
2022-09-25 20:54:24 -0400

    Updated hamburger menu code

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,14 @@
+// Declare variables
+var hamburgerButton, contactButtton, navlinks;
+
 document.addEventListener("DOMContentLoaded", () => {
   // Initialize variables
-  const hamburgerButton = document.getElementById("hamburger-button");
-  const contactButton = document.getElementById("mobile-contact-button");
-  const navlinks = document.getElementsByClassName("nav-links")[0];
-
-  // Add an event listener to check when the hamburger button is clicked
-  hamburgerButton.addEventListener("click", hamburgerMenu);
+  hamburgerButton = document.getElementById("hamburger-button");
+  contactButton = document.getElementById("mobile-contact-button");
+  navlinks = document.getElementsByClassName("nav-links")[0];
 
   // Get current styles
   console.log(getComputedStyle(navlinks));
-
-  function hamburgerMenu() {
-    // If the nav is not open
-    console.log("Display" + navlinks.style.display);
-    if (navlinks.style.display != "flex") {
-      // Open the nav and set it's properties
-    }
-
-    // If the nav is open
-    else {
-      // Close the nav
-
-      // If the screen size is large
-      if (window.innerWidth >= "1008") {
-      }
-      // If the screen size is medium
-      else if (window.innerWidth >= "641") {
-      }
-      // If the screen size is small
-      else {
-      }
-    }
-  }
 
   // Add an event listener to check when the window is resized
   window.addEventListener("resize", (event) => {
@@ -46,3 +23,48 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function hamburgerMenu(event) {
+  // If the nav is not open
+  console.log("Display" + navlinks.style.display);
+  if (navlinks.style.display != "flex") {
+    // Open the nav and set it's properties
+
+    navlinks.style.display = "flex";
+    navlinks.style.flexDirection = "column";
+    navlinks.style.position = "absolute";
+    navlinks.style.top = "6vh";
+    navlinks.style.backgroundColor = "#2B2942";
+    navlinks.style.width = "284px";
+    navlinks.style.height = "94vh";
+    navlinks.style.zIndex = "1";
+    navlinks.style.justifyContent = "start";
+    navlinks.style.rowGap = "24px";
+    navlinks.style.padding = "31px 0 0 31px";
+  }
+
+  // If the nav is open
+  else {
+    navlinks.style.display = null;
+    navlinks.style.flexDirection = null;
+    navlinks.style.position = null;
+    navlinks.style.top = null;
+    navlinks.style.backgroundColor = null;
+    navlinks.style.width = null;
+    navlinks.style.height = null;
+    navlinks.style.zIndex = null;
+    navlinks.style.justifyContent = null;
+    navlinks.style.rowGap = null;
+    navlinks.style.padding = null;
+    // Close the nav
+    // If the screen size is large
+    if (window.innerWidth >= "1008") {
+    }
+    // If the screen size is medium
+    else if (window.innerWidth >= "641") {
+    }
+    // If the screen size is small
+    else {
+    }
+  }
+}
