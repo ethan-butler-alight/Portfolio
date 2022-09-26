ace694df82bb246a56e8b3bb5292153ce810914b
ethanbtlr <4589948+ethanbtlr@users.noreply.github.com>
2022-09-25 21:36:26 -0400

    Adjusted mobile header spacing

--- scripts/main.js
+++ scripts/main.js
@@ -19,10 +19,10 @@ function openNav() {
   navlinks.style.display = "flex";
   navlinks.style.flexDirection = "column";
   navlinks.style.position = "absolute";
-  navlinks.style.top = "6vh";
+  navlinks.style.top = "5vh";
   navlinks.style.backgroundColor = "#2B2942";
   navlinks.style.width = "284px";
-  navlinks.style.height = "94vh";
+  navlinks.style.height = "95vh";
   navlinks.style.zIndex = "1";
   navlinks.style.justifyContent = "start";
   navlinks.style.rowGap = "24px";
